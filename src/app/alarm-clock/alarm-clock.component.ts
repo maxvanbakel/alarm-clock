495bc010add9640ced71145fb475cb583910b497
Max van Bakel <maxvanbakel@Maxs-MBP.fritz.box>
2021-03-24 20:01:05 +0100

    New component

---
+++ src/app/alarm-clock/alarm-clock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alarm-clock',
+  templateUrl: './alarm-clock.component.html',
+  styleUrls: ['./alarm-clock.component.scss']
+})
+export class AlarmClockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlarmClockComponent } from './alarm-clock/alarm-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlarmClockComponent
   ],
   imports: [
     BrowserModule,
